bd9f94fbb586bd5d36e11022119a8902d202ffa2
tileulessov <tleulesovsamat5@gmail.com>
2021-02-19 00:16:23 +0600

    вёрстка экрана регистрации, добавил прогресс бар в авторизацию. Также подключил firebase для авторизации и регистрации

--- app/src/main/java/com/example/bilim/sign/SignInActivity.kt
+++ app/src/main/java/com/example/bilim/sign/SignInActivity.kt
@@ -1,12 +1,22 @@
 package com.example.bilim.sign
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.bilim.R
+import com.google.firebase.auth.FirebaseAuth
+
 
 class SignInActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        initFirebaseAuth()
+    }
+
+
+    private fun initFirebaseAuth(){
+        mAuth = FirebaseAuth.getInstance();
     }
 }
