b87dd3642278c2943a0b631f2fe683113ad8fd3f
tileulessov <tleulesovsamat5@gmail.com>
2021-03-03 09:18:56 +0600

    поиск по курсам работает

--- app/src/main/java/com/example/bilim/course_page/presentation/CoursePageActivity.kt
+++ app/src/main/java/com/example/bilim/course_page/presentation/CoursePageActivity.kt
@@ -2,6 +2,10 @@ package com.example.bilim.course_page.presentation
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -19,6 +23,8 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Query
 
 class CoursePageActivity : AppCompatActivity(), CourseClickListener {
+    private lateinit var searchEditText: EditText
+    private lateinit var userNameTextView: TextView
     private lateinit var coursePageRecyclerView: RecyclerView
     private lateinit var coursePageAdapter: CoursePageAdapter
     private val mDataBase: FirebaseFirestore = FirebaseFirestore.getInstance()
@@ -28,6 +34,33 @@ class CoursePageActivity : AppCompatActivity(), CourseClickListener {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_course_page)
         initViews()
+        getCourseList("")
+        searchEditText.addTextChangedListener(object : TextWatcher {
+            lateinit var query: Query
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                query = if(s.toString().isBlank()){
+                    collectionReference
+                    searchEditText.error  = getString(R.string.search_error_text)
+                    searchEditText.requestFocus()
+                    return
+                } else{
+                    collectionReference.orderBy("courseName").startAt(s.toString()).endAt(s.toString()+"\uf8ff")
+                }
+
+                val firestoreRecyclerOptions: FirestoreRecyclerOptions<CourseNameListModel> =
+                        FirestoreRecyclerOptions.Builder<CourseNameListModel>()
+                                .setQuery(query, CourseNameListModel::class.java)
+                                .build()
+                coursePageAdapter.updateOptions(firestoreRecyclerOptions)
+            }
+
+        })
     }
 
     override fun onStart() {
@@ -40,22 +73,10 @@ class CoursePageActivity : AppCompatActivity(), CourseClickListener {
         coursePageAdapter.stopListening()
     }
 
-    private fun initViews() {
-        coursePageRecyclerView = findViewById(R.id.activity_course_content_recycler_view)
-        val query: Query = collectionReference
-        val firestoreRecyclerOptions: FirestoreRecyclerOptions<CourseNameListModel> = FirestoreRecyclerOptions.Builder<CourseNameListModel>()
-                .setQuery(query, CourseNameListModel::class.java)
-                .build()
-
-        coursePageAdapter = CoursePageAdapter(firestoreRecyclerOptions, this)
-        coursePageRecyclerView.layoutManager = LinearLayoutManager(this)
-        coursePageRecyclerView.adapter = coursePageAdapter
-    }
-
     override fun onCourseClick(
-            documentSnapshot: DocumentSnapshot,
-            position: Int,
-            model: CourseNameListModel
+        documentSnapshot: DocumentSnapshot,
+        position: Int,
+        model: CourseNameListModel
     ) {
         val id = documentSnapshot.id
         Toast.makeText(this, "Position: $position ID: $id", Toast.LENGTH_SHORT).show()
@@ -63,5 +84,23 @@ class CoursePageActivity : AppCompatActivity(), CourseClickListener {
         intent.putExtra(Constants.COURSE_NAME, model.courseName)
         intent.putExtra(Constants.COURSE_ICON, model.iconUrl)
         startActivity(intent)
+    }
+
+    private fun initViews() {
+        coursePageRecyclerView = findViewById(R.id.activity_course_content_recycler_view)
+        searchEditText = findViewById(R.id.activity_course_page_search_edit_text)
+        userNameTextView = findViewById(R.id.activity_course_page_user_name_text_view)
+    }
+
+    private fun getCourseList(text: String) {
+        val query: Query = collectionReference.orderBy("courseName").startAt(text).endAt(text+"\uf8ff")
+        val firestoreRecyclerOptions: FirestoreRecyclerOptions<CourseNameListModel> =
+            FirestoreRecyclerOptions.Builder<CourseNameListModel>()
+                .setQuery(query, CourseNameListModel::class.java)
+                .build()
+
+        coursePageAdapter = CoursePageAdapter(firestoreRecyclerOptions, this)
+        coursePageRecyclerView.layoutManager = LinearLayoutManager(this)
+        coursePageRecyclerView.adapter = coursePageAdapter
     }
 }
---
+++ app/src/main/java/com/example/bilim/course_page/presentation/CoursePageViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.bilim.course_page.presentation
+
+class CoursePageViewModel {
+}
